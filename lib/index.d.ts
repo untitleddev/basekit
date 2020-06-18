7360bfa65c7f90c0c39d6addb0d3f5a3f12eabdc
Leon van der Grient <leon@untitled.dev>
2020-06-18 14:17:40 +0200

    fix: export use device hook

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 export { default as GlobalStateProvider } from "./providers/globalState";
 export { default as useGlobalState } from "./hooks/useGlobalState";
+export { default as useDeviceInfo } from "./hooks/useDeviceInfo";
 export { default as Element } from "./ui/Element";
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { default as GlobalStateProvider } from "./providers/globalState";
 
 // Hooks
 export { default as useGlobalState } from "./hooks/useGlobalState";
+export { default as useDeviceInfo } from "./hooks/useDeviceInfo";
 
 // UI components
 export { default as Element } from "./ui/Element";
